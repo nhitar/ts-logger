baaf4e9284f3c0cffd3c9cbd71035b75d672e050
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-05-17 16:49:58 +0300

    refactor: add colour to logs

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,8 +1,26 @@
 import { config } from "./config";
 
+type LoggerLevels = "info" | "warn" | "error" | "debug" | "trace";
+
+function colourOutput(message: string, colour: string) {
+  switch(colour) {
+    case "red":
+      return `\x1b[31m${message}\x1b[0m`;
+    case "green": 
+      return `\x1b[32m${message}\x1b[0m`;
+    default:
+      return message;
+  }
+}
+
 export function createLogger() {
-  return function(level: string = "info", message: string) {
+  return function(level: LoggerLevels = "info", message: string) {
     const timestamp: string = new Date().toISOString().split('.')[0];
-    console.log(`[${config.appName} ${level}]`, timestamp, message);
+    const output = `[${config.appName} ${level}] ${timestamp} ${message}`;
+    if (level === "warn" || level === "error") {
+      console.log(colourOutput(output, "red"));
+    } else {
+      console.log(colourOutput(output, "green"));
+    }
   };
 }
