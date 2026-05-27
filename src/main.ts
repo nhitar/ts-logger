64015ad314693c3fab3ea5b45584201d83bc424c
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-05-27 17:44:12 +0300

    refactor: move scheduler function to corresponding file

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,6 @@
-﻿import { randomUUID } from 'node:crypto';
+﻿import express from 'express';
 
-import express from 'express';
-
-import { config } from './core/config';
-import { log } from './core/logger';
 import routes from './routes/routes';
-import { scheduleTask } from './services/scheduler';
-
-// function initScript() {
-//   log('INFO', 'Scheduler started.');
-//   scheduleTask(
-//     'run logger',
-//     config.intervalMsec,
-//     (() => {
-//       const requestId = randomUUID();
-//       return () => {
-//         log('INFO', 'Running.', requestId);
-//       };
-//     })(),
-//   );
-// }
-
-// initScript();
 
 const app = express();
 
--- src/services/scheduler.ts
+++ src/services/scheduler.ts
@@ -1,5 +1,8 @@
+import { randomUUID } from 'node:crypto';
+
 import { IntervalError } from '../common/errors/intervalError';
 import { TaskNameError } from '../common/errors/taskNameError';
+import { config } from '../core/config';
 import { log } from '../core/logger';
 
 export function scheduleTask(name: string, interval: number, task: Function) {
@@ -13,4 +16,18 @@ export function scheduleTask(name: string, interval: number, task: Function) {
 
   log('INFO', `Task "${name}" with interval ${interval} msec started.`);
   return setInterval(task, interval);
+}
+
+function runScheduler() {
+  log('INFO', 'Scheduler started.');
+  scheduleTask(
+    'run logger',
+    config.intervalMsec,
+    (() => {
+      const requestId = randomUUID();
+      return () => {
+        log('INFO', 'Running.', requestId);
+      };
+    })(),
+  );
 }
