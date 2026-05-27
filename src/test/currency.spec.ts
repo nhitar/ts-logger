ed2ce9f7438fe4a32e144a7c89c9d3b1bd6f2302
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-05-27 16:27:00 +0300

    feat: add tests for currencies

---
+++ src/test/currency.spec.ts
@@ -0,0 +1,84 @@
+import request from 'supertest';
+
+import { app, server } from '../main';
+
+describe('CurrencyService', () => {
+  const currencies = [
+    {
+      name: 'ticker-1',
+      ticker: 10,
+    },
+    {
+      name: 'ticker-2',
+      ticker: 20,
+    },
+  ];
+
+  beforeEach(async () => {
+    const getAllResponse = await request(app).get('/currencies');
+    for (const currency of getAllResponse.body) {
+      await request(app).delete(`/currencies/${currency.id}`);
+    }
+
+    for (let i = 0; i < currencies.length; ++i) {
+      await request(app).post('/currencies').send(currencies[i]);
+    }
+  });
+
+  it('should return all currencies', async () => {
+    const response = await request(app).get('/currencies');
+
+    expect(response.body).toMatchObject([
+      { name: 'ticker-1', ticker: 10 },
+      { name: 'ticker-2', ticker: 20 },
+    ]);
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return currency by id', async () => {
+    const response = await request(app).get('/currencies/0');
+
+    expect(response.body).toMatchObject({
+      id: 0,
+      name: 'ticker-1',
+      ticker: 10,
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should create currency', async () => {
+    const newCurrency = { name: 'ticker-3', ticker: 30 };
+    const response = await request(app).post('/currencies').send(newCurrency);
+
+    expect(response.body).toMatchObject({
+      id: 2,
+      name: 'ticker-3',
+      ticker: 30,
+    });
+    expect(response.statusCode).toBe(201);
+  });
+
+  it('should update currency', async () => {
+    const updatedCurrency = { name: 'ticker-1', ticker: 100 };
+    const response = await request(app)
+      .put('/currencies/0')
+      .send(updatedCurrency);
+
+    expect(response.body).toMatchObject({
+      id: 0,
+      name: 'ticker-1',
+      ticker: 100,
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should delete currency', async () => {
+    const response = await request(app).delete('/currencies/0');
+
+    expect(response.statusCode).toBe(204);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+});
