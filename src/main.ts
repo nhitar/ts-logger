94903727401dd0d6ab4d6f87a68400286c756bb2
Пивоев Никита <nikita.pivoev@yandex.ru>
2026-06-17 16:06:15 +0300

    feat: add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-﻿import express from 'express';
+﻿import fs from 'fs';
+
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yaml';
 
 import { log } from './core/logger';
 import database from './database/database';
@@ -19,6 +23,10 @@ app.use('/status', statusRouter);
 app.use('/auth', authRouter);
 app.use('/currencies', currencyRouter);
 app.use('/wallets', walletRouter);
+
+const swaggerYml = fs.readFileSync('openapi.yaml', 'utf8');
+const swaggerDocument = yaml.parse(swaggerYml);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 if (process.env.NODE_ENV !== 'test') {
   const intervalTasks = runScheduler();
